9cec2c418b213201f8914409966522dbcfdb623a
Johanson Onyegbula <johansononyegbula20@gmail.com>
2023-12-21 14:43:43 -0800

    Read Gridmet data

--- meadow_project.py
+++ meadow_project.py
@@ -57,7 +57,7 @@ def getBandValues(landsat_collection, point, target_date, bufferDays = 30, lands
 # reads surface reflectance values of Tier 1 collections of landsat 8 and 7
 landsat8_collection = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
 landsat7_collection = ee.ImageCollection('LANDSAT/LE07/C01/T1_SR')
-
+gridmet = ee.ImageCollection("IDAHO_EPSCOR/GRIDMET")
 
 # define arrays to store band values and landsat information
 Blue, Green, Red, NIR, SWIR_1, SWIR_2 = [], [], [], [], [], []
@@ -115,38 +115,47 @@ data.head(10)
 
 # checks how many pixels are cloud free (non-null value);
 # all bands would be simultaneously cloud-free or not
-ids = [x for x in range(2817) if not data.loc[x, 'Blue'] > 0]
+ids = [x for x in Blue if x]
 
 # write updated dataframe to new csv file
 data.to_csv('GHG_Data_Sample_Bands.csv', index=False)
 
 
+data = pd.read_csv("GHG_Data_Sample_Bands.csv")
+data.head()
+
+min_temp, max_temp = [], []
+for id in range(data.shape[0]):
+    x, y = data.loc[id, ['Long', 'Lat']]
+    point = ee.Geometry.Point(x, y)
+    target_date = data.loc[id, 'Date']
+    gridmet_filtered = gridmet.filterDate(ee.Date(target_date).advance(-16, 'day'), ee.Date(target_date).advance(16, 'day'))
+    bands = ee.Image(gridmet_filtered.first()).select(['tmmn', 'tmmx'])
+    temperature_values = bands.reduceRegion(ee.Reducer.mean(), point, 4000).getInfo()
+    tmin = temperature_values['tmmn']
+    tmax = temperature_values['tmmx']
+    
+    min_temp.append(tmin)
+    max_temp.append(tmax)
+    
+    if id%100 == 0: print(id, end=' ')
+
+data['Minimum_temperature'] = min_temp
+data['Maximum_temperature'] = max_temp
+
+data.to_csv('GHG_Data_Sample_Bands_Temp.csv', index=False)
+
+'''
 # TESTING Landsat usage from google earth engine
 # Define the point coordinates where you want to find Landsat images
 x, y = data.loc[1, ['Long', 'Lat']]
 point = ee.Geometry.Point(x, y)
-
 # read the date of the second row of dataframe, and format as string
 target_date = data.loc[1, 'Date'].strftime('%Y-%m-%d')
-
 # apply cloud masking to landsat 8 collection
 cloud_free_images = landsat8_collection.map(maskClouds)
-
 # sort cloud free images and take least cloudy one
 nearest_image = ee.Image(cloud_free_images.sort('cloud').first())
-
-# read specific bands from above image
-bands = nearest_image.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7'])
-# extract values of those bands for a single pixel in the selected landsat image
-band_values = bands.reduceRegion(ee.Reducer.mean(), point, 30).getInfo()
-
-print('Band Values:', band_values)
-
-print('Nearest image:', nearest_image)
-
-# Retrieve Landsat image metadata
-image_info = nearest_image.getInfo()
-print('Image metadata:', image_info)
 
 
 import geemap
@@ -154,7 +163,6 @@ import geemap
 # display GEE map as standard false colour composite
 map_l8 = geemap.Map(center=[y,x], zoom=10)  # note that latitude comes first here
 image_viz_params = {'bands': ['B5', 'B4', 'B3'], 'min': 0, 'max': 0.5, 'gamma': [0.95, 1.1, 1]}
-
 # Add the image layer to the map and display it.
 map_l8.add_layer(nearest_image, image_viz_params, 'false color composite')
-display(map_l8)
+display(map_l8)'''
