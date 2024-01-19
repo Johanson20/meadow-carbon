4780418a76abcfb653a0efbb5d53c06819c9966f
Johanson Onyegbula <johansononyegbula20@gmail.com>
2024-01-19 14:58:14 -0800

    Added IDs of shapefiles with low NDVIs

--- trimming_meadows.py
+++ trimming_meadows.py
@@ -1,15 +1,16 @@
 import os, ee
-from osgeo import ogr, osr
+#from osgeo import ogr, osr
 import pandas as pd
+import geopandas as gpd
 from shapely.geometry import mapping, box
 os.chdir("Code")
 
 ee.Initialize()
 
-shapefile = ogr.Open("AllPossibleMeadows_2024-01-10.shp")
-layer = shapefile.GetLayer()
-spatref = layer.GetSpatialRef()
+shapefile = gpd.read_file("AllPossibleMeadows_2024-01-19.shp")
 
+'''layer = shapefile.GetLayer()
+spatref = layer.GetSpatialRef()
 wkt = spatref.ExportToWkt()
 xycrs = osr.SpatialReference()
 xycrs.ImportFromWkt(wkt)
@@ -17,15 +18,14 @@ latlon_crs = osr.SpatialReference()
 latlon_crs.ImportFromEPSG(32611)  # WGS84
 transform = osr.CoordinateTransformation(xycrs, latlon_crs)
 
-'''landsat_image = ee.Image('LANDSAT/LC08/C01/T1/LC08_044034_20140318')
+landsat_image = ee.Image('LANDSAT/LC08/C01/T1/LC08_044034_20140318')
 im1 = landsat_image.select('B3')
 latlon_crs = im1.projection().getInfo()'''
 
-feature = layer.GetFeature(0)
-[field.GetName() for field in layer.schema]
-feature.GetField('ID')
-minx, maxx, miny, maxy = layer.GetExtent()
-nlayers = layer.GetFeatureCount()
+feature = shapefile.geometry.iloc[0]
+list(shapefile.columns)
+minx, miny, maxx, maxy = feature.bounds
+nlayers = len(shapefile)
 
 landsat8_collection = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
 
@@ -36,33 +36,31 @@ def calculate_ndvi(image):
 out_df = pd.DataFrame(columns=['ID', 'Area_m2'])
 rowId = 0
 
-for feature in layer:
-    geometry = feature.GetGeometryRef()
+for index, row in shapefile.iterrows():
+    geometry = row['geometry']
    
     # Convert the polygon to a bounding box for GEE Landsat image filtering
-    bbox = box(*geometry.GetEnvelope())
+    bbox = box(*geometry.bounds)
     bbox_coords = list(mapping(bbox)['coordinates'][0])
     
-    points = [(minx, miny), (maxx, maxy)]
+    '''points = [(minx, miny), (maxx, maxy)]
     coords = [transform.TransformPoint(x, y) for x, y in points]
     min_lat, min_lon = coords[0][:2]
-    max_lat, max_lon = coords[1][:2]
+    max_lat, max_lon = coords[1][:2]'''
 
     bbox_ee = ee.Geometry.Polygon(bbox_coords)
-    landsat_images = landsat8_collection.filterBounds(bbox_ee) \
-                                       .filterDate('2021-07-01', '2021-07-31')
-    meadow_image = landsat_images.sort("CLOUD_COVER").first()        
+    landsat_images = landsat8_collection.filterBounds(bbox_ee).filterDate('2021-07-01', '2021-07-31')
+    meadow_image = landsat_images.map(calculate_ndvi).sort("CLOUD_COVER").first()        
 
-    # Sample NDVI at the point
-    ndvi_value = meadow_image.select('NDVI') \
-        .reduceRegion(reducer=ee.Reducer.first(), geometry=bbox_ee, scale=30)
+    # Sample NDVI at the region and extract value
+    ndvi_value = meadow_image.select('NDVI').reduceRegion(ee.Reducer.mean(), bbox_ee, scale=30)
+    ndvi_number = ndvi_value.getNumber('NDVI').getInfo()
     
-    # Get the NDVI value as a number
-    ndvi_number = ndvi_value.getNumber('NDVI')
-    
-    if ndvi_number < 0.2:
-        out_df.loc[rowId] = [feature.GetField('ID'), feature.GetField('Area_m2')]
+    if not ndvi_number or ndvi_number < 0.2:
+        out_df.loc[rowId] = list(row.index)[:2]
         rowId += 1
     
-layer = None
+    if index%100==0: print(index, end=" ")
+    
+shapefile = None
 out_df.to_csv('False_meadows.csv', index=False)
