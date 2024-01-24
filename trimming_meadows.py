8173a0aba96d7d10512436a9a5464a2a0e228edd
Johanson Onyegbula <jonyegbula@pointblue.org>
2024-01-24 15:15:02 -0800

    Added NDWIs

--- trimming_meadows.py
+++ trimming_meadows.py
@@ -17,11 +17,12 @@ nlayers = len(shapefile)
 landsat8_collection = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2").filterDate('2022-07-01', '2022-07-31')
 # landsat9_collection = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2").filterDate('2022-07-01', '2022-07-31')
 
-def calculate_ndvi(image):
+def calculate_indices(image):
     ndvi = image.normalizedDifference(['SR_B5', 'SR_B4']).rename('NDVI')
-    return image.addBands(ndvi)
+    ndwi = image.normalizedDifference(['SR_B5', 'SR_B6']).rename('NDWI')
+    return image.addBands([ndvi, ndwi])
 
-out_df = pd.DataFrame(columns=['ID', 'Area_m2', 'Area_km2', 'NDVI'])
+out_df = pd.DataFrame(columns=['ID', 'Area_m2', 'Area_km2', 'NDVI', 'NDWI'])
 rowId = 0
 
 for index, row in shapefile.iterrows():
@@ -34,7 +35,7 @@ for index, row in shapefile.iterrows():
     bbox_ee = ee.Geometry.Polygon(bbox_coords)
     
     landsat_images = landsat8_collection.filterBounds(bbox_ee)
-    meadow_image = calculate_ndvi(landsat_images.sort("CLOUD_COVER").first())
+    meadow_image = calculate_indices(landsat_images.sort("CLOUD_COVER").first())
     
     if feature.geom_type == 'Polygon':
         shapefile_bbox = ee.Geometry.Polygon(list(feature.exterior.coords))
@@ -43,12 +44,12 @@ for index, row in shapefile.iterrows():
     
     landsat_masked = meadow_image.updateMask(meadow_image.clip(shapefile_bbox))
     # Sample NDVI at the region and extract value
-    ndvi_value = landsat_masked.select('NDVI').reduceRegion(reducer=ee.Reducer.mean(), geometry=bbox_ee, scale=30)
-    ndvi_number = ndvi_value.getNumber('NDVI').getInfo()
+    indices = landsat_masked.select(['NDVI', 'NDWI']).reduceRegion(ee.Reducer.mean(), bbox_ee, 30).getInfo()
+    ndvi_number, ndwi = list(indices.values())
     
-    if not ndvi_number or ndvi_number < 0.2:
+    if not ndvi_number or not ndwi or (ndvi_number < 0.2 and ndwi < 0.5):
         val = list(row.values)[:3]
-        val.append(ndvi_number)
+        val += [ndvi_number, ndwi]
         out_df.loc[rowId] = val
         rowId += 1
     
