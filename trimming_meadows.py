ceb07e297a47108b8c29c73a259ff3068f44b2c6
Johanson Onyegbula <jonyegbula@pointblue.org>
2024-01-25 16:07:23 -0800

    Indices calculated within shapefile bbox

--- trimming_meadows.py
+++ trimming_meadows.py
@@ -19,7 +19,7 @@ landsat8_collection = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2").filterDate('2
 
 def calculate_indices(image):
     ndvi = image.normalizedDifference(['SR_B5', 'SR_B4']).rename('NDVI')
-    ndwi = image.normalizedDifference(['SR_B5', 'SR_B6']).rename('NDWI')
+    ndwi = image.normalizedDifference(['SR_B3', 'SR_B5']).rename('NDWI')
     return image.addBands([ndvi, ndwi])
 
 out_df = pd.DataFrame(columns=['ID', 'Area_m2', 'Area_km2', 'NDVI', 'NDWI'])
@@ -27,24 +27,16 @@ rowId = 0
 
 for index, row in shapefile.iterrows():
     feature = row['geometry']
-   
-    # Convert the polygon to a bounding box for GEE Landsat image filtering
-    bbox = box(*feature.bounds)
-    bbox_coords = list(mapping(bbox)['coordinates'][0])
-    
-    bbox_ee = ee.Geometry.Polygon(bbox_coords)
-    
-    landsat_images = landsat8_collection.filterBounds(bbox_ee)
-    meadow_image = calculate_indices(landsat_images.sort("CLOUD_COVER").first())
-    
+
     if feature.geom_type == 'Polygon':
         shapefile_bbox = ee.Geometry.Polygon(list(feature.exterior.coords))
     elif feature.geom_type == 'MultiPolygon':
         shapefile_bbox = ee.Geometry.MultiPolygon(list(list(poly.exterior.coords) for poly in feature.geoms))
     
-    landsat_masked = meadow_image.updateMask(meadow_image.clip(shapefile_bbox))
-    # Sample NDVI at the region and extract value
-    indices = landsat_masked.select(['NDVI', 'NDWI']).reduceRegion(ee.Reducer.mean(), bbox_ee, 30).getInfo()
+    landsat_image = landsat8_collection.filterBounds(shapefile_bbox).sort("CLOUD_COVER").first()
+    meadow_image = calculate_indices(landsat_image)
+    
+    indices = meadow_image.select(['NDVI', 'NDWI']).reduceRegion(ee.Reducer.mean(), shapefile_bbox, 30).getInfo()
     ndvi_number, ndwi = list(indices.values())
     
     if not ndvi_number or not ndwi or (ndvi_number < 0.2 and ndwi < 0.5):
