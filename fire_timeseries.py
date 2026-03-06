c5296c48b639a4a56436f952085179993460f4ce
Johanson Onyegbula <jonyegbula@pointblue.org>
2026-03-05 18:58:24 -0800

    Rectified time series

--- fire_timeseries.py
+++ fire_timeseries.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 from joblib import Parallel, delayed
 
 mydir = "C:/Users/jonyegbula/Documents/PointBlue/Code"
@@ -20,7 +21,7 @@ warnings.filterwarnings("ignore")
 
 def computeStats(values):
     # function to calculate the slope, mean and sample standard deviation of time series values within a range of years
-    stats = [[np.nan]*3]*len(values)
+    stats = [[np.nan for _ in range(3)] for __ in range(len(values))]
     ans = []
     for val in values:
         if len(values[val]) > 1:
@@ -29,11 +30,17 @@ def computeStats(values):
     return stats
 
 
-# load shapefile of burn summaries and csv file of burned meadows (touching or within) already generated
+# load shapefile of burn summaries and csv file of burned meadows (entirely within) already generated
 epsg_crs = "EPSG:4326"
 meadows = gpd.read_file("files/meadowsFiresCombos_1984to2024_20251208.shp").to_crs(epsg_crs)
+meadows = meadows[meadows.rltnshp == "entirely_within"]
+meadows.drop_duplicates(subset=['UniquID', 'YEAR_'], inplace=True)
 meadows.sort_values(by="YEAR_", inplace=True)
+meadows.reset_index(drop=True, inplace=True)
 data = pd.read_csv("csv/burned_meadows_1984_2025.csv")
+data = data[data.Description == "entirely_within"]
+data.drop_duplicates(subset=['UniqueID', 'FireYear'], inplace=True)
+data.reset_index(drop=True, inplace=True)
 # select relevant columns and unique IDs for timeseries
 uniqueIds = list(set(data.UniqueID))
 mycols = ['X', 'Y', 'ANPP', 'BNPP', 'Rh', 'NEP']
@@ -43,9 +50,8 @@ years = list(range(1984, 2025))
 # create empty dataframe for storing statistics
 all_cols = ['UniqueID', 'CalFireID', 'Relationship', 'FireYear']
 for col in cols:
-    all_cols.extend([f'{col}_Slope_Bfr_Fire', f'{col}_Mean_Bfr_Fire', f'{col}_Std_Bfr_Fire', 
-                     f'{col}_Slope_Aft_Fire', f'{col}_Mean_Aft_Fire', f'{col}_Std_Aft_Fire'])
-meadow_data = pd.DataFrame(index=np.arange(meadows.shape[0]), columns=all_cols)
+    all_cols.extend([f'{col}_Slope_Bfr_Fire', f'{col}_Mean_Bfr_Fire', f'{col}_Std_Bfr_Fire', f'{col}_Slope_Aft_Fire', f'{col}_Mean_Aft_Fire', f'{col}_Std_Aft_Fire'])
+meadow_data = pd.DataFrame(columns=all_cols)
 
 
 def generateFireTimeSeries(uniqueId):
@@ -54,6 +60,7 @@ def generateFireTimeSeries(uniqueId):
     meadow = meadows[meadows.UniquID == uniqueId].iloc[0]
     meadow_geom = gpd.GeoDataFrame([meadow], geometry='geometry', crs=epsg_crs)
     minx, miny, maxx, maxy = meadow_geom.total_bounds
+    ans = []    # final result/row(s) to be returned
     vals = {}   # initialize dictionary with values for each relevant column
     for col in cols:
         vals[col] = {}
@@ -74,61 +81,82 @@ def generateFireTimeSeries(uniqueId):
         if stats.empty: continue
         for col in cols:
             vals[col][year] = stats[col].values[0]
-        if year%10 == 0: print(year, end=', ')    # print progress every decade
-
-    if not vals or len(vals) < 10:    # skip processing if insufficient valid values for geometry was found
+        # if year%10 == 0: print(year, end=', ')    # print progress every decade
+    del chunk, filtered, df, pixels_gdf, joined, meadow_geom
+    
+    if not vals:    # skip processing if no valid values for geometry was found
         print(f"{uniqueId} returned no valid values!")
-        return
+        return ans
     
-    ans = []
-    for col in cols: 
-        # extract fire dates for feature geometry for determining date range of timeseries analysis
-        target_dates = iter([yr for yr in feature.FireYear])
-        startyear = 1984
-        target_date = next(target_dates, None)
-        timerange, fireyears = {}, []
-        while target_date:  # iterate through each unique fire date of the meadow and get stats between dates
-            year = int(target_date)
-            fireyears.append(target_date)
-            yrs = range(startyear, year+1)
-            yCol = [vals[col][yr] for yr in yrs]
-            timerange[yrs] = yCol
-            startyear = year+1
-            target_date = next(target_dates, None)
-        timerange[range(year+1, 2025)] = [vals[col][yr] for yr in range(startyear, 2025)]
-        # compute stats for each time range
-        results = computeStats(timerange)
-    
-        # plot time series graph
-        plt.figure(figsize=(8, 5))
-        plt.plot(years, vals[col].values(), marker="o")
-        # Plot vertical line for each fire year
-        for yr in fireyears:
-            plt.axvline(yr, linestyle="--")
-        # Legend with stats
-        legend_lines = []
-        for firedate, result in zip(fireyears, results):
-            slope, mean, std = result
-            legend_lines.append(f"Before {firedate}: slope = {slope:.2f}, mean = {mean:.2f}, stdev = {std:.2f}")
-        slope, mean, std = results[-1]
-        legend_lines.append(f"After {firedate}: slope = {slope:.2f}, mean = {mean:.2f}, stdev = {std:.2f}")
-        legend_text = "\n".join(legend_lines)
-        
-        plt.legend([legend_text])
-        plt.title(f"{col} Time Series for {uniqueId}")
-        plt.xlabel("Year")
-        plt.ylabel(f"{col}")
-        plt.tight_layout()
-        plt.savefig(f"files/fire/{uniqueId}_{col}_fire_timeseries.png", dpi=300, bbox_inches="tight")
-        plt.show()
-        # store records for dataframe
+    try:
+        # prepare results for each unique fire year
         for idx in range(len(set(feature.FireYear))):
             fire = feature.iloc[idx]
-            ans.append([fire.UniqueID, fire.CalFireID, fire.Description, fire.FireYear] + results[idx] + results[idx+1])
+            ans.append([meadow.UniquID, meadow.CalFrID, fire.Description, fire.FireYear])
+        # write plots to a pdf
+        with PdfPages(f"files/fire/{uniqueId}_fire_timeseries.pdf") as pdf:
+            for col in cols: 
+                # extract fire dates for feature geometry for determining date range of timeseries analysis
+                target_dates = iter([yr for yr in feature.FireYear])
+                startyear = 1984
+                target_date = next(target_dates, None)
+                timerange, fireyears = {}, []
+                while target_date:  # iterate through each unique fire date of the meadow and get stats between dates
+                    year = int(target_date)
+                    fireyears.append(target_date)
+                    yrs = [yr for yr in range(startyear, year+1) if yr in vals[col]]
+                    yCol = [vals[col][yr] for yr in yrs]
+                    timerange[tuple(yrs)] = yCol
+                    startyear = year+1
+                    target_date = next(target_dates, None)
+                timerange[tuple([yr for yr in range(year+1, 2025) if yr in vals[col]])] = [vals[col][yr] for yr in range(startyear, 2025)]
+                # compute stats for each time range
+                results = computeStats(timerange)
             
+                # plot time series graph
+                plt.figure(figsize=(8, 6))
+                plt.plot([yr for yr in years if yr in vals[col]], vals[col].values(), marker="o")
+                # Plot vertical line for each fire year
+                for yr in fireyears:
+                    plt.axvline(yr, linestyle="--")
+                # Legend with stats
+                legend_lines = []
+                for firedate, result in zip(fireyears, results):
+                    slope, mean, std = result
+                    legend_lines.append(f"Before {firedate}: slope = {slope:.2f}, mean = {mean:.2f}, stdev = {std:.2f}")
+                slope, mean, std = results[-1]
+                legend_lines.append(f"After {firedate}: slope = {slope:.2f}, mean = {mean:.2f}, stdev = {std:.2f}")
+                legend_text = "\n".join(legend_lines)
+                plt.title(legend_text, fontsize=10)
+                plt.xlabel("Year", fontweight='bold')
+                plt.ylabel(f"{col}", fontweight='bold')
+                plt.tight_layout()
+                pdf.savefig()
+                plt.close()
+            # store records for dataframe
+            try:
+                for idx in range(len(set(feature.FireYear))):
+                    fire = feature.iloc[idx]
+                    ans[idx].extend((results[idx] + results[idx+1]))
+            except:     # error expected to be thrown if fire occurs in 2024 (no data after)
+                pass
+        print(uniqueId, end=', ')
+    except:
+        print(f"{uniqueId} threw an error!")
+    
     return ans
 
 
-# generateFireTimeSeries("SNM175")
+# generateFireTimeSeries("SNM6702")
 with Parallel(n_jobs=18, prefer="threads") as parallel:
-    result = parallel(delayed(uniqueId)(uniqueId) for uniqueId in uniqueIds)
+    results = parallel(delayed(generateFireTimeSeries)(uniqueId) for uniqueId in uniqueIds)
+
+# write finals rows from results into csv file
+for idx in range(len(results)):
+    result = results[idx]
+    if result and len(result) > 1:
+        for res in result:
+            if len(res) > 4: meadow_data.iloc[len(meadow_data), :] = res
+    else:
+        if result and len(result) > 4: meadow_data.iloc[len(meadow_data), :] = result
+meadow_data.to_csv("csv/burned_meadows_statistics.csv", index=False)
